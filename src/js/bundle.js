e73c12649c56e56c432f7b879331d63ec37219e9
vrbdas <vrbdas@gmail.com>
2023-07-11 00:24:32 +0600

    11.07.2023

---
+++ src/js/bundle.js
@@ -0,0 +1,12 @@
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!**************************!*\
+  !*** ./src/js/script.js ***!
+  \**************************/
+
+
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
