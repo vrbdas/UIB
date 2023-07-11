f4ff2981ac4239d58b5b2d54f100a7511cd9f1de
vrbdas <vrbdas@gmail.com>
2023-07-12 00:27:15 +0600

    add licence slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,14 @@
-'use strict';
+import {tns} from 'tiny-slider';
+
+const slider = tns({
+  container: '.licence__tns',
+  items: 3,
+  slideBy: 1,
+  autoplay: false,
+  nav: false,
+  loop: false,
+  gutter: 20,
+  prevButton: '.licence__slider-prev',
+  nextButton: '.licence__slider-next',
+});
 
